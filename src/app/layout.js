78ecb5e0cd8bd8e3948a3f9c2e470a86b286e371
Plievore <paulo_lievore@hotmail.com>
2026-02-18 19:54:47 -0300

    analytics vercel include

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import SmoothScroll from "@/components/SmoothScroll";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
